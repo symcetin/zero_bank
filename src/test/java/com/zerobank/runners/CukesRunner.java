65dc03fb2d2b076200c71e71153bb649aafb539a
symcetin <seyma.oskurcu@gmail.com>
2020-05-01 16:13:20 -0400

    commit

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features ="src/test/resources/features",
         glue = "com/zerobank/step_definitions",
-        tags = "@statements_documents",
+        tags = "",
         dryRun = false,
         plugin = "html:target/default-report"
 )
