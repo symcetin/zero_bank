5310a841067f9afdb144a6417fc555b36e644907
symcetin <seyma.oskurcu@gmail.com>
2020-05-02 15:15:27 -0400

    updated

--- src/test/java/com/zerobank/pages/PageBase.java
+++ src/test/java/com/zerobank/pages/PageBase.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class PageBase {
+public abstract class PageBase {
 
     protected WebDriver driver = Driver.getDriver();
     protected WebDriverWait wait = new WebDriverWait(driver, 25);
